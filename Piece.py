33f797969dfdffa78d3dc39efe29b46be93ab4b0
srushti3 <sck@pdx.edu>
2019-05-26 14:15:33 -0700

    Piece class completed

---
+++ Piece.py
@@ -0,0 +1,204 @@
+import numpy as np
+import math
+
+class Piece:
+    
+    def __init__(self, cells):
+        self.cells = cells
+        self.dimension = cells.shape[0]
+        self.row = 0 
+        self.col = 0
+
+    
+    def select_piece(index):
+        
+        if index == 0:
+            piece = Piece([[3,3],[3,3]])
+        
+        elif index == 1:
+            piece = Piece([[1,0,0],[1,1,1],[0,0,0]])
+            
+        elif index == 2:
+            piece = Piece([[0,0,2],[2,2,2],[0,0,0]])
+            
+        elif index == 3:
+             piece = Piece([[4,4,0],[0,4,4],[0,0,0]])
+                
+        elif index == 4:
+             piece = Piece([[0,5,5],[5,5,0],[0,0,0]])
+                
+        elif index == 5:
+             piece = Piece([[0,6,0],[6,6,6],[0,0,0]])
+                
+        elif index == 6:
+             piece = Piece([[0,0,0,0],[7,7,7,7],[0,0,0,0],[0,0,0,0]])
+        
+        piece.row = 0
+        piece.col = math.floor((10 - piece.dimension) / 2)
+        return piece
+    
+    
+    def clone(self):
+        copy_cells = np.empty_like (self.cells)
+        copy_cells[:] = self.cells
+        piece = Piece(copy_cells)
+        piece.row = self.cells.shape[0]
+        piece.colum = self.cells.shape[1]
+        return piece
+        
+    
+    def checkLeftMove(self, board):
+        for row in range(len(self.cells)):
+            for col in range(len(self.cells[row])):
+                pos_row = self.row + row
+                pos_col = self.col - 1
+                if self.cells[row][col] != 0 :
+                    if not (pos_col >= 0 and board.cells[pos_row][pos_col] == 0):
+                        return False
+                
+        return True       
+        
+    
+    
+    def checkRightMove(self, board):
+        for row in range(len(self.cells)):
+            for col in range(len(self.cells[row])):
+                pos_row = self.row + row
+                pos_col = self.col + 1
+                if self.cells[row][col] != 0 :
+                    if not (pos_col >= 0 and board.cells[pos_row][pos_col] == 0):
+                        return False
+                
+        return True   
+       
+    
+    
+    def checkDownMove(self, board):
+        for row in range(len(self.cells)):
+            for col in range(len(self.cells[row])):
+                pos_row = self.row + row + 1
+                pos_col = self.col + col
+                if self.cells[row][col] != 0  and pos_row >= 0 :
+                    if not (pos_row < board.rows and board.cells[pos_row][pos_col] == 0):
+                        return False
+                
+        return True   
+       
+   
+    def moveLeft(self, board):
+        if not self.checkLeftMove(board):
+            return False
+    
+        self.col = self.col - 1
+        return True
+        
+    
+    def moveRight(self, board):
+        if not self.checkRightMove(board):
+            return False
+    
+        self.col = self.col + 1
+        return True
+        
+    
+    def moveDown(self, board):
+        if not self.checkDownMove(board):
+            return False
+    
+        self.row = self.row + 1
+        return True
+    
+    def rotateCells(self):
+        copy_cell = np.empty_like(self.cells)
+        
+        if self.dimension == 2:
+            copy_cell[0][0] = self.cells[1][0]
+            copy_cell[0][1] = self.cells[0][0]
+            copy_cell[1][0] = self.cells[1][1]
+            copy_cell[1][1] = self.cells[0][1]
+            
+        elif self.dimension == 3:
+            copy_cell[0][0] = self.cells[2][0]
+            copy_cell[0][1] = self.cells[1][0]
+            copy_cell[0][2] = self.cells[0][0]
+            copy_cell[1][0] = self.cells[2][1]
+            copy_cell[1][1] = self.cells[1][1]
+            copy_cell[1][2] = self.cells[0][1]
+            copy_cell[2][0] = self.cells[2][2]
+            copy_cell[2][1] = self.cells[1][2]
+            copy_cell[2][2] = self.cells[0][2]
+            
+        elif self.dimension == 4:
+            copy_cell[0][0] = self.cells[3][0]
+            copy_cell[0][1] = self.cells[2][0]
+            copy_cell[0][2] = self.cells[1][0]
+            copy_cell[0][3] = self.cells[0][0]
+            copy_cell[1][3] = self.cells[0][1]
+            copy_cell[2][3] = self.cells[0][2]
+            copy_cell[3][3] = self.cells[0][3]
+            copy_cell[3][2] = self.cells[1][3]
+            copy_cell[3][1] = self.cells[2][3]
+            copy_cell[3][0] = self.cells[3][3]
+            copy_cell[2][0] = self.cells[3][2]
+            copy_cell[1][0] = self.cells[3][1]
+
+            copy_cell[1][1] = self.cells[2][1]
+            copy_cell[1][2] = self.cells[1][1]
+            copy_cell[2][2] = self.cells[1][2]
+            copy_cell[2][1] = self.cells[2][2]
+            
+        self.cells = copy_cell
+        
+    def calculateOffset(self, board):
+        temp_piece = self.clone()
+        
+        temp_piece.rotateCells()
+        
+        if board.isValidPiece(temp_piece):
+            return {rowOffset: temp_piece.row - self.row, columnOffset: temp_piece.col - self.col}
+        
+        initialRow = temp_piece.row
+        initialCol = temp_piece.col
+        
+        
+        for i in range(self.dimension):
+            temp_piece.col = initialCol + i
+            if board.isValidPiece(temp_piece):
+                return {rowOffset: temp_piece.row - self.row, columnOffset: temp_piece.col - self.col}
+            
+            for j in range(self.dimension):
+                temp_piece.row = initialRow - j
+                if board.isValidPiece(temp_piece):
+                    return {rowOffset: temp_piece.row - self.row, columnOffset: temp_piece.col - self.col}
+            temp_piece.row = initialRow
+            
+        temp_piece.col = initialCol
+        
+        
+        for i in range(self.dimension):
+            temp_piece.column = initialCol - i
+            if board.isValidPiece(temp_piece):
+                return {rowOffset: temp_piece.row - self.row, columnOffset: temp_piece.column - self.column}
+            
+            for j in range(self.dimension):
+                temp_piece.row = initialRow - j
+                if board.isValidPiece(temp_piece):
+                    return {rowOffset: temp_piece.row - self.row, columnOffset: temp_piece.column - self.column}
+            temp_piece.row = initialRow
+            
+        temp_piece.column = initialCol
+        
+        
+        return None
+    
+    def rotate(self, board):
+        offset = self.calculateOffset(board)
+        
+        if offset != None:
+            self.rotateCells(board)
+            self.row = self.row + offset.rowOffset
+            self.column = self.column + offset.columnOffset
+                    
+                
+    
+        
